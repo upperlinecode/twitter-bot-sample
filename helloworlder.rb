d659fc8bb324bda69b74398774f611f053a09f63
dfenjves <fenjves.daniel@gmail.com>
2015-12-28 13:28:49 -0500

    first commit

--- helloworlder.rb
+++ helloworlder.rb
@@ -1,11 +1,12 @@
 require 'pry'
 require 'twitter'
+require_relative './env'
 
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "9zMFVhdRheN0HsIYJR3l1jKkb"
-  config.consumer_secret     = "pa0DU7IZ3Jq0wNdzxCKj8BzJWOPKv3C46SDhlhc7LFD6kPWz6N"
-  config.access_token        = "4634713395-wfagsUpPG1FHa9oFG8SiN5aguyT4Ig0qDTDgsia"
-  config.access_token_secret = "Ph05UNpsTlFIQ2PxjPhY7TSSQAAUfmvg7ixumsdjuMTuf"
+  config.consumer_key        = CONSUMER_KEY
+  config.consumer_secret     = CONSUMER_SECRET
+  config.access_token        = ACCESS_TOKEN
+  config.access_token_secret = ACCESS_TOKEN_SECRET
 end
 
 languages = ["مرحبا بالعالم!", "zdravo svijete!", "Здравей, свят!", "Hola, món!", "世界您好！", "zdravo svijete!", "Ahoj světe!", "Hej verden!", "Hallo mensen!"]
