c9f4245f5fd5dd80682b4e3d3e326fef2ed4d6b4
dfenjves <fenjves.daniel@gmail.com>
2015-12-28 15:06:37 -0500

    added dotenv

--- helloworlder.rb
+++ helloworlder.rb
@@ -1,12 +1,12 @@
 require 'bundler'
 Bundler.require
-require_relative './env'
+Dotenv.load
 
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = CONSUMER_KEY
-  config.consumer_secret     = CONSUMER_SECRET
-  config.access_token        = ACCESS_TOKEN
-  config.access_token_secret = ACCESS_TOKEN_SECRET
+  config.consumer_key        = ENV["CONSUMER_KEY"]
+  config.consumer_secret     = ENV["CONSUMER_SECRET"]
+  config.access_token        = ENV["ACCESS_TOKEN"]
+  config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
 end
 
 languages = ["مرحبا بالعالم!", "zdravo svijete!", "Здравей, свят!", "Hola, món!", "世界您好！", "zdravo svijete!", "Ahoj světe!", "Hej verden!", "Hallo mensen!"]
